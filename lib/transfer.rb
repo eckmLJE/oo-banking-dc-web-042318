b759772e86882ae0fd02725f0768da465f1cd381
eckmLJE <lucas.eckman@gmail.com>
2018-04-27 16:24:55 -0400

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,11 +14,22 @@ class Transfer
     self.sender.valid? && self.receiver.valid?
   end
 
+  # def execute_transaction
+  #   if self.sender.balance < self.amount
+  #     self.status = "rejected"
+  #     "Transaction rejected. Please check your account balance."
+  #   elsif self.valid? && self.status == "pending"
+  #     self.sender.balance -= self.amount
+  #     self.receiver.balance += self.amount
+  #     self.status = "complete"
+  #   else
+  #     self.status = "rejected"
+  #     "Transaction rejected. Please check your account balance."
+  #   end
+  # end
+
   def execute_transaction
-    if self.sender.balance < self.amount
-      self.status = "rejected"
-      "Transaction rejected. Please check your account balance."
-    elsif self.valid? && self.status == "pending"
+    if self.valid? && self.status == "pending"
       self.sender.balance -= self.amount
       self.receiver.balance += self.amount
       self.status = "complete"
